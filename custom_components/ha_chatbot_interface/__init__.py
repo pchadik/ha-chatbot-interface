f8d6c2d134552b7471928b11f69a9d977f0f60c0
Paul Chadik <pchadik@gmail.com>
2023-03-22 19:21:20 -0400

    renaming file and cleaning up init

--- custom_components/ha_chatbot_interface/__init__.py
+++ custom_components/ha_chatbot_interface/__init__.py
@@ -3,8 +3,10 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .sensor import DOMAIN  # Add this import
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setup(entry, DOMAIN)
     )
     return True
